84efebaaae262c79e5fe9520bc32892300abd76a
Matthew Black <matthewallenblack@gmail.com>
2019-11-12 23:06:05 -0600

    Three happy lil' fellas, now.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-console.log('js sourced.')
+alert("Be sure to click the sad lil' fella a few times.")
 
+clickCount = 0
 document.addEventListener('click', function (e) {
   el = e.target
   targetId = e.target.id
@@ -10,7 +11,23 @@ document.addEventListener('click', function (e) {
       el.textContent = ':)'
     }
     else {
-      el.textContent = ':('
+      if (clickCount < 2) {
+        newSmiley = document.createElement('h1')
+        newSmiley.textContent = ':)'
+        newSmiley.setAttribute('class', 'smile')
+        e.target.parentElement.appendChild(newSmiley)
+        clickCount += 1
+      } else if (clickCount < 3) {
+        love = document.createElement('h1')
+        love.textContent = "Aww. Three happy lil' fellas, now."
+        e.target.parentElement.appendChild(love)
+        clickCount += 1
+      } else if (clickCount < 4) {
+        lol = document.createElement('h1')
+        lol.textContent = "What a snazzy website!"
+        e.target.parentElement.appendChild(lol)
+        clickCount += 1
+      }
     }
   }
 
